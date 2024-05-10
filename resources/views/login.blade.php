bdf055f5bcd20ca3ba082164b66bdc6b609101e3
Marvel280904 <128071190+Marvel280904@users.noreply.github.com>
2024-05-10 18:45:08 +0700

    Add files via upload

---
+++ resources/views/login.blade.php
@@ -0,0 +1,79 @@
+@extends("template.main")
+
+@section("title", "JUDUL")
+
+@section('body')
+<div class="row d-flex align-items-center justify-content-center" style="min-height: 500px">
+<form method="POST" style="max-width: 400px">
+  @csrf
+    <!-- Name input -->
+    <div class="mb-4">
+      <label class="form-label" for="form2Example1">Name</label>
+      <input type="text" class="form-control @error('name')
+                is-invalid
+            @enderror" name="name"
+            @if (old('name') != null)
+                value="{{ old('name') }}"
+            @endif >
+    </div>
+
+    <!-- Email input -->
+    <div class="mb-4">
+      <label class="form-label" for="form2Example1">Email address</label>
+      <input type="email" id="form2Example1" class="form-control" name="email"
+        @if (old('email') != null)
+            value="{{ old('email') }}"
+        @endif>
+    </div>
+
+    <!-- Password input -->
+    <div class="mb-4">
+      <label class="form-label" for="form2Example2">Password</label>
+      <input type="password" id="form2Example2" class="form-control" name="password"
+      @if (old('password') != null)
+            value="{{ old('password') }}"
+        @endif>
+    </div>
+
+    <!-- 2 column grid layout for inline styling -->
+    <div class="row mb-4">
+      <div class="col d-flex justify-content-center">
+        <!-- Checkbox -->
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+          <label class="form-check-label" for="form2Example31"> Remember me </label>
+        </div>
+      </div>
+
+      <div class="col">
+        <!-- Simple link -->
+        <a href="#!">Forgot password?</a>
+      </div>
+    </div>
+
+    <!-- Submit button -->
+    <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign in</button>
+
+    {{-- <!-- Register buttons -->
+    <div class="text-center">
+      <p>Not a member? <a href="#!">Register</a></p>
+      <p>or sign up with:</p>
+      <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
+        <i class="fab fa-facebook-f"></i>
+      </button>
+
+      <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
+        <i class="fab fa-google"></i>
+      </button>
+
+      <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
+        <i class="fab fa-twitter"></i>
+      </button>
+
+      <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
+        <i class="fab fa-github"></i>
+      </button>
+    </div> --}}
+  </form>
+</div>
+@endsection
